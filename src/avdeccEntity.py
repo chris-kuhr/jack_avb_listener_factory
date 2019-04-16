6eed26a2371428e44f040bcecb4deeaa08172201
Christoph Kuhr <christoph.kuhr@hs-anhalt.de>
2019-04-16 02:29:56 +0200

    create methods for jack client creation, todo signal/slot comm with background thread, subclassing table item for header rotation

--- src/avdeccEntity.py
+++ src/avdeccEntity.py
@@ -1,6 +1,7 @@
 class AVDECCEntity(object):
     def __init__(self):        
         self.idx = -1
+        self.jackclient_name = ""
         self.name = "noname"
         self.entityId = b'aabbcc0011ddeeff'
         self.firmwareVersion = "0"
@@ -13,7 +14,7 @@ class AVDECCEntity(object):
     #-------------------------------------------
 
     def encodeString(self):
-        return "%d;%s;%s;%s;%s;%s;%d;%d;%s;%s;" %( self.idx, self.name, self.entityId.decode("utf8"), self.firmwareVersion, self.MACAddr.decode("utf8"), self.endpointType, self.channelCount, self.sampleRate_k, self.destMAC.decode("utf8"), self.streamId.decode("utf8")) 
+        return "%d;%s;%s;%s;%s;%s;%d;%d;%s;%s;" %( self.idx, self.jackclient_name, self.name, self.entityId.decode("utf8"), self.firmwareVersion, self.MACAddr.decode("utf8"), self.endpointType, self.channelCount, self.sampleRate_k, self.destMAC.decode("utf8"), self.streamId.decode("utf8")) 
     #-------------------------------------------
 
 
@@ -23,15 +24,16 @@ class AVDECCEntity(object):
             self.idx = int(propList[0])
         except ValueError:            
             return -1
-        self.name = propList[1]
-        self.entityId = bytearray(propList[2].encode("utf8"))
-        self.firmwareVersion = propList[3]
-        self.MACAddr = bytearray(propList[4].encode("utf8"))
-        self.endpointType = propList[5]
-        self.channelCount = int(propList[6])
-        self.sampleRate_k = int(propList[7])
-        self.destMAC = bytearray(propList[8].encode("utf8"))
-        self.streamId = bytearray(propList[9].encode("utf8"))        
+        self.jackclient_name = propList[1]
+        self.name = propList[2]
+        self.entityId = bytearray(propList[3].encode("utf8"))
+        self.firmwareVersion = propList[4]
+        self.MACAddr = bytearray(propList[5].encode("utf8"))
+        self.endpointType = propList[6]
+        self.channelCount = int(propList[7])
+        self.sampleRate_k = int(propList[8])
+        self.destMAC = bytearray(propList[9].encode("utf8"))
+        self.streamId = bytearray(propList[10].encode("utf8"))        
         return 1
     #-------------------------------------------
 
--- src/jackAVBfactory.py
+++ src/jackAVBfactory.py
@@ -1,5 +1,5 @@
 '''
-Created on Jul 21, 2014
+Created on April 16, 2019
 
 @author: christoph
 '''
@@ -13,17 +13,12 @@ import posix_ipc
 import ipc_utils as utils
 from avdeccEntity import AVDECCEntity, serializeList2Str, deserializeStr2List
 from avdecccmdlineWrapper import AVDECC_Controller
-
-#from mainWindow import Ui_MainWindow
   
 headerBGColor = QtGui.QColor(125,125,125)  
 connectedBGColor = QtGui.QColor(0,255,0) 
 connectingBGColor = QtGui.QColor(128,229,255) 
 defaultBGColor = QtGui.QColor(255,255,255) 
-
-
-headerFontColor = QtGui.QColor(255,255,255)  
-defaultFontColor = QtGui.QColor(0,0,0)   
+cornerBGColor = QtGui.QColor(120,0,0) 
 
 class JackAVBfactory(QtWidgets.QMainWindow):
     '''Main Class'''
@@ -44,48 +39,25 @@ class JackAVBfactory(QtWidgets.QMainWindow):
 
         self.semaphore.release()
         
+        self.listeners = []
+        self.talkers = []
+        
         '''
         self.avdecccmdline_wrapper_thread()
         '''
              
-        self.cols = 17   
+        self.cols = 1
         self.rows = 1 
-        self.index = 0
-        self.endpointType = "Listener"
-        
-        self.namesT = ["","T1","T2","T3","T4","T5","T6","T7","T8","T9","T10","T11","T12","T13","T14","T15","T16"]
-        
-        self.namesL = [""]
-        
+        self.endpointType = "l"
+                
         self.qt.tableWidget.setColumnCount(self.cols)
         self.qt.tableWidget.setRowCount(self.rows)
         
-        self.qt.tableWidget.verticalHeader().setVisible(False)
         self.qt.tableWidget.horizontalHeader().setVisible(False)
+        self.qt.tableWidget.verticalHeader().setVisible(False)
         
-        for i in range(1,self.cols):                    # set col width
-            self.qt.tableWidget.setColumnWidth(i,50)                       
-                       
-        for i in range(1,self.cols):                    # set col label
-            item = QtWidgets.QTableWidgetItem()         
-            item.setText(self.namesT[i])
-            item.setBackground(headerBGColor)
-            #item.itemClicked.connect(self.showEntityDetails(int))
-            self.qt.tableWidget.setItem(0, i, item)                       
-                       
-        for j in range(1,self.rows):                    # set row label
-            item = QtWidgets.QTableWidgetItem()
-            item.setText(self.namesL[j])
-            item.setBackground(headerBGColor)
-            self.qt.tableWidget.setItem(j, 0, item)
-                       
-        for j in range(1,self.rows):                    # populate table
-#            self.qt.tableWidget.insertRow(j)    
-            for i in range(1,self.cols):
-                item = QtWidgets.QTableWidgetItem()
-                item.setText('O')
-                item.setBackground(defaultBGColor)
-                self.qt.tableWidget.setItem(j, i, item)
+        self.qt.tableWidget.setItem(0, 0, QtWidgets.QTableWidgetItem("")) 
+        self.qt.tableWidget.item(0, 0).setBackground(cornerBGColor)
                         
         self.qt.tableWidget.cellClicked.connect(self.cellClickedSlot)
         self.qt.radioButton.toggled.connect(self.qt.setEndpointTypeTalker)
@@ -96,43 +68,92 @@ class JackAVBfactory(QtWidgets.QMainWindow):
     
     def setEndpointTypeListener(self, state):
         if state:
-            self.endpointType = "Listener"
-            print(self.endpointType)
+            self.endpointType = "l"
     #-------------------------------------------------------------------------------------------------------------------------
-    
-    
+        
     def setEndpointTypeTalker(self, state):
         if state:
-            self.endpointType = "Talker"
-            print(self.endpointType)
+            self.endpointType = "t"
+    #-------------------------------------------------------------------------------------------------------------------------
+            
+    def addEntityToTable(self, entity, m, n):
+#        self.qt.textEdit.clear()
+        self.qt.textEdit.append( "###########################################################" )
+        self.qt.textEdit.append( "Creating JACK AVB %s: %s"%( entity.endpointType, entity.jackclient_name) )
+        self.qt.textEdit.append( "MAC Address: %s"%( entity.MACAddr.decode("utf8") ) )
+        self.qt.textEdit.append( "Stream Id: %s"%( entity.streamId.decode("utf8") ) )
+        self.qt.textEdit.append( "Stream Destination MAC: %s"%( entity.destMAC.decode("utf8") ) )
+        self.qt.textEdit.append( "Channel Count: %d"%( entity.channelCount ) )
+                
+        if entity.endpointType == "l":  
+            self.rows += 1   
+            self.qt.tableWidget.insertRow(m)   
+            self.qt.tableWidget.setRowCount(m+1)
+        elif entity.endpointType == "t":
+            self.cols += 1
+            self.qt.tableWidget.insertColumn(n) 
+            self.qt.tableWidget.setColumnCount(n+1) 
+                
+        item = QtWidgets.QTableWidgetItem()
+        item.setBackground(headerBGColor)        
+        item.setText(entity.jackclient_name + entity.name) 
+        self.qt.tableWidget.setItem(m, n, item)
+                          
+        if entity.endpointType == "l":  
+            for i in range(1,self.cols):        
+                self.qt.tableWidget.setItem(m, i, QtWidgets.QTableWidgetItem('O'))
+                self.qt.tableWidget.item(m, i).setBackground(defaultBGColor)
+        elif entity.endpointType == "t":
+            for i in range(1,self.rows):        
+                self.qt.tableWidget.setItem(i, n, QtWidgets.QTableWidgetItem('O'))
+                self.qt.tableWidget.item(i, n).setBackground(defaultBGColor)
     #-------------------------------------------------------------------------------------------------------------------------
     
     def createJACKClient(self):
-#        self.qt.textEdit.clear()
-        self.qt.textEdit.append( "Creating JACK AVB %s: %s"%( self.endpointType, str(self.qt.lineEdit_3.text()) ) )
-        self.qt.textEdit.append( "AVB Device Name: %s"%( str(self.qt.lineEdit_4.text()) ) )
-        self.qt.textEdit.append( "MAC Address: %s"%( str(self.qt.lineEdit_5.text()) ) )
-        self.qt.textEdit.append( "Stream Id: %s"%( str(self.qt.lineEdit.text()) ) )
-        self.qt.textEdit.append( "Stream Destination MAC: %s"%( str(self.qt.lineEdit_2.text()) ) )
-        self.qt.textEdit.append( "Channel Count: %d"%( int(self.qt.spinBox.value()) ) )
-        self.qt.tableWidget.insertRow(self.rows)   
+        entity =  AVDECCEntity()     
+        entity.jackclient_name = str(self.qt.lineEdit_3.text())
+        entity.name = ""
+        entity.entityId = bytearray( ("".join( str(self.qt.lineEdit.text()).split(":") ) ).encode("utf8") )
+        entity.firmwareVersion = ""
+        entity.MACAddr = bytearray( ("".join( str(self.qt.lineEdit_5.text()).split(":") ) ).encode("utf8") )
+        entity.endpointType = self.endpointType
+        entity.channelCount = int(self.qt.spinBox.value())
+        entity.sampleRate_k = 48000
+        entity.destMAC = bytearray( ("".join( str(self.qt.lineEdit_2.text()).split(":") ) ).encode("utf8") )
+        entity.streamId = bytearray( ("".join( str(self.qt.lineEdit.text()).split(":") ) ).encode("utf8") )
         
-        item = QtWidgets.QTableWidgetItem()
-        item.setText(str(self.qt.lineEdit_3.text()))
-        item.setBackground(headerBGColor)
-        self.qt.tableWidget.setItem(self.rows, 0, item)
-          
-        for i in range(1,self.cols):
-            item = QtWidgets.QTableWidgetItem()
-            item.setText('O')
-            item.setBackground(defaultBGColor)
-            self.qt.tableWidget.setItem(self.rows, i, item)
-            
-        self.rows += 1
-        self.qt.tableWidget.setRowCount(self.rows) 
+        if entity.endpointType == "l":    
+            entity.idx = self.rows     
+            self.listeners.append(entity)        
+            self.addEntityToTable(entity, entity.idx, 0)
+        elif entity.endpointType == "t":    
+            entity.idx = self.cols
+            self.talkers.append(entity)
+            self.addEntityToTable(entity, 0, entity.idx)
     #-------------------------------------------------------------------------------------------------------------------------
-    
-    
+        
+    def updateAVBEntityList(self):
+        self.semaphore.acquire()
+        serStr = utils.read_from_memory(self.mapfile)
+        self.semaphore.release()
+
+        serList = deserializeStr2List(serStr)
+
+        for device in serList:    
+            entity = AVDECCEntity()  
+            if entity.decodeString(device) > 0: # return values -1, 1
+                if "t" in entity.endpointType:   
+                    entity.idx = self.cols               
+                    self.talkers.append(entity)
+                    self.addEntityToTable(entity, 0, self.cols)
+                if "l" in entity.endpointType:   
+                    entity.idx = self.rows            
+                    self.listeners.append(entity)
+                    self.addEntityToTable(entity, self.rows, 0)
+            else:
+                break
+    #-------------------------------------------------------------------------------------------------------------------------
+     
     def cellClickedSlot(self, i, j):
         print(i,j)    
         if j == 0 or i == 0:
@@ -140,8 +161,7 @@ class JackAVBfactory(QtWidgets.QMainWindow):
         else:
             self.connectStream(i, j)
     #-------------------------------------------------------------------------------------------------------------------------
-       
-    
+           
     def connectStream(self, i, j):
         if str(self.qt.tableWidget.item(i,j).text()) == 'O': 
             for n in range(1,self.cols):
@@ -149,10 +169,12 @@ class JackAVBfactory(QtWidgets.QMainWindow):
                 self.qt.tableWidget.item(i,n).setBackground(defaultBGColor)
             self.qt.tableWidget.item(i,j).setText('...')
             self.qt.tableWidget.item(i,j).setBackground(connectingBGColor)
+            self.qt.textEdit.append( "l -..-..-..-> t" )
             self.qt.textEdit.append( "JACK AVB Listener Client: %s waiting for connection to AVB Talker %s"%( self.qt.tableWidget.item(i,0).text(), self.qt.tableWidget.item(0,j).text()) )
         elif str(self.qt.tableWidget.item(i,j).text()) == '...': 
             self.qt.tableWidget.item(i,j).setText('X')
             self.qt.tableWidget.item(i,j).setBackground(connectedBGColor)
+            self.qt.textEdit.append( "l ----------> t" )
             self.qt.textEdit.append( "Activating JACK AVB Listener Client: %s and connecting to AVB Talker %s"%( self.qt.tableWidget.item(i,0).text(), self.qt.tableWidget.item(0,j).text()) )
         elif str(self.qt.tableWidget.item(i,j).text()) == 'X':  
         
@@ -161,6 +183,7 @@ class JackAVBfactory(QtWidgets.QMainWindow):
                 self.qt.tableWidget.item(i,n).setBackground(defaultBGColor)
             self.qt.tableWidget.item(i,j).setText('O')
             self.qt.tableWidget.item(i,j).setBackground(defaultBGColor)
+            self.qt.textEdit.append( "l xxxxxxxxxxxx t" )
             self.qt.textEdit.append( "Disconnecting JACK AVB Listener Client: %s from AVB Talker %s and dectivating JACK Client"%( self.qt.tableWidget.item(i,0).text(), self.qt.tableWidget.item(0,j).text()) )
     #-------------------------------------------------------------------------------------------------------------------------
        
@@ -179,24 +202,7 @@ class JackAVBfactory(QtWidgets.QMainWindow):
         self.avdecc_thread.started.connect(self.avdeccctl.run_avdecccmdline_thread)
         self.avdecc_thread.start() 
     #-------------------------------------------------------------------------------------------------------------------------
-       
-    def updateEntityList(self):
-        self.semaphore.acquire()
-        serStr = utils.read_from_memory(self.mapfile)
-        self.semaphore.release()
-
-        serList = deserializeStr2List(serStr)
-
-        entity_list2 = []
-        for device in serList:    
-            dummy = AVDECCEntity()  
-            if dummy.decodeString(device) > 0: # return values -1, 1
-                entity_list2.append(dummy)
-                print(dummy.encodeString())
-            else:
-                break
-    #-------------------------------------------------------------------------------------------------------------------------
-       
+              
     def closeEvent(self, event):
 
         quit_msg = "Are you sure you want to exit the program?"
