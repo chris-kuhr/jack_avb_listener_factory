815b8aeb9ac3f6c88375bde9a91e7f71c14923fa
Christoph Kuhr <christoph.kuhr@hs-anhalt.de>
2019-06-27 21:00:30 +0200

    try wrapper fix thread asyncio loop

--- src/WebsocketController.py
+++ src/WebsocketController.py
@@ -35,7 +35,7 @@ class WebsocketController():
         
         
         #self.avdeccctl = AVDECC_Controller("enp1s0", cmd_path ="/home/christoph/source_code/github-kuhr/OpenAvnu.git/avdecc-lib/controller/app/cmdline/avdecccmdline")
-        self.avdeccctl = AVDECC_Controller(argv, "enp1s0", cmd_path ="opt/OpenAvnu/avdecc-lib/controller/app/cmdline/avdecccmdline")
+        self.avdeccctl = AVDECC_Controller(argv, "enp1s0", cmd_path ="/opt/OpenAvnu/avdecc-lib/controller/app/cmdline/avdecccmdline")
         self.avdeccctl.start()
         
         self.running = False
